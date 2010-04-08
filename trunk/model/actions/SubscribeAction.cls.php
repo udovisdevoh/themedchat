45c677a3d64a543ef7ee3f68508bb0f2ae216c56
glacasse <glacasse@76902d0c-f41d-f7bc-4452-bd41e0cc9bee>
2010-04-08 23:15:31 +0000

    Subscribe: works! pw: m*n****y

--- trunk/model/actions/SubscribeAction.cls.php
+++ trunk/model/actions/SubscribeAction.cls.php
@@ -1,8 +1,26 @@
 <?php
 class SubscribeAction extends AbstractAction
 {
+	private $result = "";
+	
 	public function doAction()
 	{
+		if (!$_POST['user'] || !$_POST['password'])
+			return;
+			
+		$soapClient = new nusoap_client('http://b63server.notes-de-cours.com/services.php', false);
+		$err = $soapClient->getError();
+		if ($err) {
+			echo "(Erreur : )" . $err;
+		}
+		
+		//pw: munymuny
+		$this->result = $soapClient->call('enregistrer', array('matricule' => '0171066','prenom' => 'Guillaume','nom' => 'Lacasse','nomUsager' => $_POST['user'], 'motDePasse' => md5($_POST['password'])));		
+	}
+	
+	public function getResult()
+	{
+		return $this->result;
 	}
 }	
 ?>
