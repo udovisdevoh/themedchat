2add62144834100c5675a9c1aa2b02a2037959be
glacasse <glacasse@76902d0c-f41d-f7bc-4452-bd41e0cc9bee>
2010-04-08 22:17:23 +0000

    Ajouté menu pour theme 1 et 2, pour 3, ça ne marche pas encore

--- model/HeaderAndAssemblyInfo.ns.php
+++ model/HeaderAndAssemblyInfo.ns.php
@@ -7,5 +7,7 @@
 
 require_once('viewers/ChatInputViewer.cls.php');
 require_once('viewers/LoginViewer.cls.php');
+require_once('viewers/SubscribeViewer.cls.php');
+require_once('viewers/MenuViewer.cls.php');
 
 ?>
--- model/viewers/SubscribeViewer.cls.php
+++ model/viewers/SubscribeViewer.cls.php
@@ -1,12 +1,12 @@
 <?php
-class LoginViewer
+class SubscribeViewer
 {
 	public function getHtmlCode()
 	{
 		$html = "";
 		
 		$html .= '<div id="chatPage">';
-			$html .= '<h1>Se connecter</h1>';
+			$html .= '<h1>S\'enregistrer</h1>';
 			$html .= '<form method="post" action="'.$_SERVER['REQUEST_URI'].'">';
 				$html .= '<div class="FormElement" id="formElementOutput">';
 
@@ -14,7 +14,7 @@ class LoginViewer
 					$html .= '<div>Mot de passe: <input type="text" name="password" /></div>';
 					
 					$html .= '<div>';
-						$html .= '<input class="Button" type="submit" value="Entrer" />';
+						$html .= '<input class="Button" type="submit" value="S\'enregistrer" />';
 					$html .= '</div>';
 				
 				$html .= '</div>';
