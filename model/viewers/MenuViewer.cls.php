7e4779acb4425251a03b667d62bf1a3fa0dccdf9
glacasse <glacasse@76902d0c-f41d-f7bc-4452-bd41e0cc9bee>
2010-04-08 23:54:52 +0000

    Unsubscribe: works!

--- model/viewers/MenuViewer.cls.php
+++ model/viewers/MenuViewer.cls.php
@@ -9,6 +9,7 @@ class MenuViewer
 		$menuItem[] = '<a href="login.php">Se connecter</a>';
 		$menuItem[] = '<a href="logout.php">Se déconnecter</a>';
 		$menuItem[] = '<a href="subscribe.php">S\'enregistrer</a>';
+		$menuItem[] = '<a href="unsubscribe.php">Se désenregistrer</a>';
 		
 		$html .= '<div class="Clear"></div><div class="Menu">'.implode(" | ", $menuItem).'</div>';
 		
