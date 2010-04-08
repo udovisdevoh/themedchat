bf53e3c706216077cc90603b3880a9227eaffea2
glacasse <glacasse@76902d0c-f41d-f7bc-4452-bd41e0cc9bee>
2010-04-08 22:30:10 +0000

    Séparé headers d'assembly info Ajout lien ver page logout qui n'exite pas encore fixé theme miami

---
+++ model/AssemblyInfo.ns.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once('viewers/ChatInputViewer.cls.php');
+require_once('viewers/LoginViewer.cls.php');
+require_once('viewers/SubscribeViewer.cls.php');
+require_once('viewers/MenuViewer.cls.php');
+
+?>
--- model/viewers/MenuViewer.cls.php
+++ model/viewers/MenuViewer.cls.php
@@ -7,6 +7,7 @@ class MenuViewer
 		
 		$menuItem[] = '<a href=".\">Chat</a>';
 		$menuItem[] = '<a href="login.php">Se connecter</a>';
+		$menuItem[] = '<a href="logout.php">Se déconnecter</a>';
 		$menuItem[] = '<a href="subscribe.php">S\'enregistrer</a>';
 		
 		$html .= '<div class="Clear"></div><div class="Menu">'.implode(" | ", $menuItem).'</div>';
