54a4ae19810536ba9c58661444297ef590d88ea8
glacasse <glacasse@76902d0c-f41d-f7bc-4452-bd41e0cc9bee>
2010-04-08 19:24:44 +0000

    Convertir architechture: work in progress

---
+++ model/HeaderAndAssemblyInfo.ns.php
@@ -0,0 +1,10 @@
+<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+<title>Chat</title>
+<link href="./css/chat.css" rel="stylesheet" type="text/css" media="screen" />		
+<script type="text/javascript" src="./javaScript/script.js"></script>
+
+<?php
+
+require_once('viewers/ChatInputViewer.cls.php');
+
+?>
