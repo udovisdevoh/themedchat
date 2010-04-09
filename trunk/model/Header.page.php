683b6848cd8829c0e86a26c5d4ccfbc410f0d881
glacasse <glacasse@76902d0c-f41d-f7bc-4452-bd41e0cc9bee>
2010-04-09 15:35:32 +0000

    Ajouté fichier script ajax vide, renamé référence vers

--- trunk/model/Header.page.php
+++ trunk/model/Header.page.php
@@ -2,4 +2,4 @@
 <title>Chat</title>
 <link href="./css/chat.css" rel="stylesheet" type="text/css" media="screen" />		
 <script type="text/javascript" src="./javaScript/script.js"></script>
-<script type="text/javascript" src="../model/chat.js"></script>
+<script type="text/javascript" src="../model/Chat.js"></script>
