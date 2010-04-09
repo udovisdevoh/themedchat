0b457446aff4082553f50cbc328919c8c91adeb5
glacasse <glacasse@76902d0c-f41d-f7bc-4452-bd41e0cc9bee>
2010-04-09 13:56:48 +0000

    On peut maintenant voir (sans ajax) les messages déjà écrits

--- trunk/model/actions/ChatAction.cls.php
+++ trunk/model/actions/ChatAction.cls.php
@@ -3,6 +3,8 @@ class ChatAction extends AbstractAction
 {
 	private $memberList;
 	
+	private $messageList;
+	
 	public function doAction()
 	{
 		$soapClient = new nusoap_client('http://b63server.notes-de-cours.com/services.php', false);
@@ -12,11 +14,17 @@ class ChatAction extends AbstractAction
 		}
 		
 		$this->memberList = $soapClient->call('listeDesMembres', array('clef' => $_SESSION['chatKey']));		
+		$this->messageList = $soapClient->call('lireMessages', array('clef' => $_SESSION['chatKey']));		
 	}
 	
 	public function getMemberList()
 	{
 		return $this->memberList;
+	}
+	
+	public function getMessageList()
+	{
+		return $this->messageList;
 	}
 }	
 ?>
