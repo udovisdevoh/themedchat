bd6ea2aea7ebaae060401eab139a170845d9c881
glacasse <glacasse@76902d0c-f41d-f7bc-4452-bd41e0cc9bee>
2010-04-09 16:19:17 +0000

    Script AJAX coté serveur: works!

--- model/actions/GetMemberListAction.cls.php
+++ model/actions/GetMemberListAction.cls.php
@@ -5,6 +5,16 @@ class GetMemberListAction extends AbstractAction
 	
 	public function doAction()
 	{
+		$soapClient = new nusoap_client('http://b63server.notes-de-cours.com/services.php', false);
+		$err = $soapClient->getError();
+		if ($err)
+		{
+			echo "(Erreur : )" . $err;
+		}
+		
+		sleep(1);
+		
+		$this->ajaxData = json_encode($soapClient->call('listeDesMembres', array('clef' => $_SESSION['chatKey'])));
 	}
 	
 	public function getAjaxData()
